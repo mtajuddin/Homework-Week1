819ebe699384dc43b498f694ea7740aca966d04f
mtajuddin <mtajuddin@cca.edu>
2016-01-26 09:28:37 -0800

    puzzle3-yay
    I struggled with the usage of the getColor function while writing the code for this puzzle. Fortunately, I remembered that I could create a variable with the value of the getColor function(+ a parameter) and use that variable's name as a parameter for the setColor function.

---
+++ puzzle3.js
@@ -0,0 +1,37 @@
+// Help Rudy the Red Dot collect all the green dots!
+
+// Control Rudy with the following commands:
+//  up();                   -- moves Rudy up one square
+//  down();                 -- ...down one square
+//  left();                 -- ...left one square
+//  right();                -- ...right one square
+
+// Interact with Rudy's environment using the following commands:
+//  remainingDots();        -- returns the number of green dots remaining
+//  getColor();             -- returns the color of Rudy's square
+//  setColor(color);        -- changes the color of Rudy's square, opens gates if colors match
+
+function twoDown() {
+  down();
+  down();
+}
+
+function downThree(total) {
+  var times = 0;
+  while (times < total) {
+    down();
+    times = times + 1;
+  }
+}
+
+
+twoDown();
+getColor();
+var color = getColor(color);
+downThree(3);
+right();
+right();
+setColor(color);
+right();
+right();
+up();
